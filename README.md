e714b32eeb9740c4c35f2070012eaf7502b3e59a
RomanKhrabatyn <romankhr1974@gmail.com>
2024-11-12 21:32:05 +0200

    Reporting was added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # tradenation-cypressMocha
+
+1. Clone project
+git clone 
+
+2. Install dependencies
+npm install
+
+3. Run tests
+ npx cypress run --browser chrome
+
+4. Open report
+mochawesome.html from root directory in a browser
--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,57 @@
 const { defineConfig } = require("cypress");
+const fs = require("fs-extra");
+const path = require("path");
+const { execSync } = require("child_process");
+
+function deleteReportsFolder() {
+  const reportsPath = path.join(__dirname, "cypress/reports");
+
+  if (fs.existsSync(reportsPath)) {
+    fs.readdirSync(reportsPath).forEach((file) => {
+      const curPath = path.join(reportsPath, file);
+      if (file !== "merged-report.json" && fs.statSync(curPath).isFile()) {
+        console.log(`Deleting file: ${curPath}`);
+        fs.unlinkSync(curPath);
+      }
+    });
+  }
+}
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // You can add any event listeners here if needed
+      on("before:run", () => {
+        deleteReportsFolder();
+      });
+      on("after:run", () => {
+        try {
+          execSync("npm run merge-report", { stdio: "inherit" });
+        } catch (error) {
+          console.error("Failed to run the merge-report script:", error);
+        }
+      });
+      on("after:screenshot", (details) => {
+        console.log(`Screenshot taken: ${details.path}`);
+        const videoPath = details.test.screenshotPath.replace("screenshots", "videos").replace(".png", ".mp4");
+        console.log(`Video path: ${videoPath}`);
+      });
     },
-    specPattern: "cypress/e2e/**/*.js",
+    specPattern: "cypress/e2e/**/*.js", 
+    reporter: "cypress-multi-reporters", 
+    reporterOptions: {
+      reporterEnabled: "mochawesome", 
+      mochawesomeReporterOptions: {
+        reportDir: "cypress/reports", 
+        overwrite: false, 
+        html: true, 
+        json: true,
+        inline: true, 
+      },
+    },
+    video: true,
+    screenshotsFolder: "cypress/screenshots", 
+    videosFolder: "cypress/videos", 
+    trashAssetsBeforeRuns: true, 
+    screenshotOnRunFailure: true, 
   },
 });
